f37ab54b70174a997f3f0b6939e7c90c4bef34d1
abfloresc <abfloresc@unjbg.edu.pe>
2024-06-24 02:30:33 -0500

    ejercicio2.cpp

---
+++ ejercicio2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1,n2,c=0, i=1;
+    cin>>n1;
+    cin>>n2;
+    while(i<=n1&&n2<=n2){
+        if(n1%i==0&&n2%i==0){
+            c=c+1;
+        }
+        i=i+1;
+    }
+    if(c>1){
+        cout<<"No son PESI";
+    } else{
+        cout<<"Son PESI";
+    }
+    }
