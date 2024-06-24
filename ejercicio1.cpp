6140ca0040469e4e24bd77c93883ae21a5d1c44f
abflorec <abfloresc@unjbg.edu.pe>
2024-06-24 01:50:05 -0500

    Actualizacion del ejercicio1:agrege una condicion y lo volvi mas atractivo al usuario

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
     float a,b,r;
     char c;
-    cin>>a;
-    cin>>b;cin>>c;
+    cout<<"ingrese el primer numero";cin>>a;
+    cout<<"ingrese el segundo numero"cin>>b;cin>>c;
     switch(c){
         case '+':
             r=a+b;
@@ -18,10 +18,14 @@ int main(){
             r=a*b;
             cout<<r;
         break;
-        case '/': 
+        case '/':
+            if(b!=0){
                 r=a/b;
                 cout<<r;
-            
+            }
+            else{
+                cout<<"no se puede";
+            }
         break;
         default: 
             cout<<"operacion no valida";
