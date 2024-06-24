c52daacc209f4f7364d359ca909961640e477171
abfloresc <abfloresc@unjbg.edu.pe>
2024-06-24 02:48:57 -0500

    ejercicio2.cpp
    Actualizacion del ejercicio 2, mejorando la la estetica y mostrando de mejor manera las abreviaciones

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int main(){
     int n1,n2,c=0, i=1;
-    cin>>n1;
-    cin>>n2;
+    cout<<"ingrese un numero : ";cin>>n1;
+    cout<<"ingrese otro numero: "; cin>>n2;
     while(i<=n1&&n2<=n2){
         if(n1%i==0&&n2%i==0){
-            c=c+1;
-        }
+            c=c+1; }
         i=i+1;
     }
     if(c>1){
         cout<<"No son PESI";
     } else{
         cout<<"Son PESI";
-    }
+    } return 0
     }
